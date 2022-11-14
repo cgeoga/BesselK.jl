8319acd1b02a80e325479e3c6d693ba2c8e0e02a
Chris Geoga <cgeoga@protonmail.com>
2022-11-14 16:58:53 -0500

    Fix norm -> _norm is matern function.

--- src/matern.jl
+++ src/matern.jl
@@ -2,6 +2,8 @@
 # A standard Matern covariance function. This is not the best parameterization,
 # but it is the most popular, so here we are.
 
+_norm(t) = sqrt(sum(z->z*z, t))
+
 """
 matern(x, y, params)
 
@@ -13,7 +15,7 @@ For more information, see Stein (1999), Interpolation of Spatial Data: Some Theo
 """
 function matern(x, y, params)
   (sg, rho, nu) = (params[1], params[2], params[3])
-  dist = norm(x-y)
+  dist = _norm(x-y)
   _iszero(dist) && return sg^2
   arg = sqrt(2*nu)*dist/rho
   (sg*sg*(2^(1-nu))/_gamma(nu))*adbesselkxv(nu, arg)
